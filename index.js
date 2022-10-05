5b31263977c96e31d240bcb287f00d47db4d9f22
Karan <rkrana811@gmail.com>
2022-10-04 21:31:28 -0500

    completed the app
    need to add footer

---
+++ index.js
@@ -0,0 +1,35 @@
+const dob = document.querySelector("#dobInput");
+const luckyNumber = document.querySelector("#inputLucky");
+const button = document.querySelector(".checkNumber");
+const message = document.querySelector("#para");
+
+
+
+const calculateSum = (date) => {
+    let sum = 0;
+    date = date.replaceAll("-", "");
+    for (let digit of date) {
+        sum = sum + Number(digit);
+    }
+    return sum;
+}
+
+const checkIfNumberLucky = (sumOfDate, numberToCheck) => {
+    console.log(sumOfDate, numberToCheck);
+    if (sumOfDate % numberToCheck === 0) {
+        message.innerText = "It's a lucky number!";
+    }
+    message.innerText = "Number is not that lucky."
+}
+
+button.addEventListener('click', () => {
+    const date = dob.value;
+    const numberToCheck = luckyNumber.value;
+    if (date && numberToCheck) {
+        const sumOfDate = calculateSum(date);
+        checkIfNumberLucky(sumOfDate, numberToCheck);
+    } else {
+        message.innerText = "Please enter both fields.";
+    }
+});
+
